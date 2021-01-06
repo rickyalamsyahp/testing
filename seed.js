f7fe6e426b1e24de99a8b2396eeff763ddb135a7
rickyalamsyahp <42854402+rickyalamsyahp@users.noreply.github.com>
2021-01-06 10:39:35 +0700

    deploy heroku

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 
 // Connect to MongoDB via Mongoose
 seeder.connect(
-  "mongodb://127.0.0.1:27017/db_staycation",
+  "mongodb+srv://rickyAlamsyah:Salsaricky8511@cluster0.fvm5i.mongodb.net/db_staycation?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useCreateIndex: true,
