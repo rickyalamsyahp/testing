932bcffd0384f399ba2a3672f55503143db52fe5
rickyalamsyahp <42854402+rickyalamsyahp@users.noreply.github.com>
2021-01-06 20:31:06 +0700

    cros lagi lagi

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var usersRouter = require("./routes/users");
 const adminRouter = require("./routes/admin");
 const apiRouter = require("./routes/api");
 
-app.use(cors(corsOptions));
+
 
 var app = express();
 
@@ -62,7 +62,7 @@ app.use("/users", usersRouter);
 // admin
 app.use("/admin", adminRouter);
 app.use("/api/v1/member", apiRouter);
-
+app.use(cors(corsOptions));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
