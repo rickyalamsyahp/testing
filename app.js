ee08441bd06ccdab68247cbab976987e748c2f2c
rickyalamsyahp <42854402+rickyalamsyahp@users.noreply.github.com>
2021-01-06 20:02:56 +0700

    edit express

--- app.js
+++ app.js
@@ -27,6 +27,15 @@ const apiRouter = require("./routes/api");
 
 var app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -56,7 +65,7 @@ app.use("/users", usersRouter);
 // admin
 app.use("/admin", adminRouter);
 app.use("/api/v1/member", apiRouter);
-app.use(cors());
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
