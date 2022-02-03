196504a753b1bb6de837b4899e5668482d5be943
Ricky <rickyalamsyahp@gmail.com>
2022-02-03 16:57:47 +0700

    fix app

--- app.js
+++ app.js
@@ -47,10 +47,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
+// app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 // admin
 // app.use('/admin', adminRouter);
 app.use('/api/v1',apiRouter);
