bff7e5600fc4ab5c6f7c5b943bf3ad626b74f420
Ricky <rickyalamsyahp@gmail.com>
2022-02-03 16:53:20 +0700

    setcors

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ const apiRouter = require('./routes/api');
 
 var app = express();
 
-app.use(cors());
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"
+}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -50,7 +53,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // admin
 // app.use('/admin', adminRouter);
-app.use('/api/v1',cors() ,apiRouter);
+app.use('/api/v1',apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
